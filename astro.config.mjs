88a9d97e3530d9e2e8e3ac0acab09775ec400006
Patrik Bednář <patrikbednar06@gmail.com>
2026-02-11 17:55:09 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://www.jdj-craft.cz',
-  output: 'hybrid',
+  
+  output: 'static',
+  
   adapter: vercel(),
+  
   integrations: [
     tailwind(), 
     sitemap(),
